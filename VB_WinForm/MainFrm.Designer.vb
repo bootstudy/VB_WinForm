81caef13c9c592c44196f23bd31c58a132b3b4e6
kumakun_2019@hotmail.com <kumakun_2019@hotmail.com>
2019-06-04 21:24:43 +0900

    プロジェクト ファイルを追加します。

---
+++ VB_WinForm/MainFrm.Designer.vb
@@ -0,0 +1,149 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class MainFrm
+    Inherits System.Windows.Forms.Form
+
+    'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows フォーム デザイナーで必要です。
+    Private components As System.ComponentModel.IContainer
+
+    'メモ: 以下のプロシージャは Windows フォーム デザイナーで必要です。
+    'Windows フォーム デザイナーを使用して変更できます。  
+    'コード エディターを使って変更しないでください。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txt_URL = New System.Windows.Forms.TextBox()
+        Me.WebBrowser1 = New System.Windows.Forms.WebBrowser()
+        Me.btnSure = New System.Windows.Forms.Button()
+        Me.TabControl1 = New System.Windows.Forms.TabControl()
+        Me.TabPage1 = New System.Windows.Forms.TabPage()
+        Me.TabPage2 = New System.Windows.Forms.TabPage()
+        Me.txtDisplay = New System.Windows.Forms.TextBox()
+        Me.btn_Exec = New System.Windows.Forms.Button()
+        Me.TabControl1.SuspendLayout()
+        Me.TabPage1.SuspendLayout()
+        Me.TabPage2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'txt_URL
+        '
+        Me.txt_URL.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.txt_URL.Location = New System.Drawing.Point(0, 0)
+        Me.txt_URL.Name = "txt_URL"
+        Me.txt_URL.Size = New System.Drawing.Size(792, 19)
+        Me.txt_URL.TabIndex = 0
+        '
+        'WebBrowser1
+        '
+        Me.WebBrowser1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.WebBrowser1.Location = New System.Drawing.Point(3, 25)
+        Me.WebBrowser1.MinimumSize = New System.Drawing.Size(20, 20)
+        Me.WebBrowser1.Name = "WebBrowser1"
+        Me.WebBrowser1.Size = New System.Drawing.Size(786, 370)
+        Me.WebBrowser1.TabIndex = 1
+        '
+        'btnSure
+        '
+        Me.btnSure.Location = New System.Drawing.Point(717, 401)
+        Me.btnSure.Name = "btnSure"
+        Me.btnSure.Size = New System.Drawing.Size(75, 23)
+        Me.btnSure.TabIndex = 2
+        Me.btnSure.Text = "確認"
+        Me.btnSure.UseVisualStyleBackColor = True
+        '
+        'TabControl1
+        '
+        Me.TabControl1.Controls.Add(Me.TabPage1)
+        Me.TabControl1.Controls.Add(Me.TabPage2)
+        Me.TabControl1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.TabControl1.Location = New System.Drawing.Point(0, 0)
+        Me.TabControl1.Name = "TabControl1"
+        Me.TabControl1.SelectedIndex = 0
+        Me.TabControl1.Size = New System.Drawing.Size(800, 450)
+        Me.TabControl1.TabIndex = 3
+        '
+        'TabPage1
+        '
+        Me.TabPage1.Controls.Add(Me.WebBrowser1)
+        Me.TabPage1.Controls.Add(Me.btnSure)
+        Me.TabPage1.Controls.Add(Me.txt_URL)
+        Me.TabPage1.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage1.Name = "TabPage1"
+        Me.TabPage1.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage1.Size = New System.Drawing.Size(792, 424)
+        Me.TabPage1.TabIndex = 0
+        Me.TabPage1.Text = "Browser"
+        Me.TabPage1.UseVisualStyleBackColor = True
+        '
+        'TabPage2
+        '
+        Me.TabPage2.Controls.Add(Me.btn_Exec)
+        Me.TabPage2.Controls.Add(Me.txtDisplay)
+        Me.TabPage2.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage2.Name = "TabPage2"
+        Me.TabPage2.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage2.Size = New System.Drawing.Size(792, 424)
+        Me.TabPage2.TabIndex = 1
+        Me.TabPage2.Text = "ConsoleText"
+        Me.TabPage2.UseVisualStyleBackColor = True
+        '
+        'txtDisplay
+        '
+        Me.txtDisplay.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.txtDisplay.Location = New System.Drawing.Point(3, 3)
+        Me.txtDisplay.Multiline = True
+        Me.txtDisplay.Name = "txtDisplay"
+        Me.txtDisplay.Size = New System.Drawing.Size(786, 389)
+        Me.txtDisplay.TabIndex = 0
+        '
+        'btn_Exec
+        '
+        Me.btn_Exec.Location = New System.Drawing.Point(711, 398)
+        Me.btn_Exec.Name = "btn_Exec"
+        Me.btn_Exec.Size = New System.Drawing.Size(75, 23)
+        Me.btn_Exec.TabIndex = 1
+        Me.btn_Exec.Text = "実行"
+        Me.btn_Exec.UseVisualStyleBackColor = True
+        '
+        'MainFrm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.TabControl1)
+        Me.Name = "MainFrm"
+        Me.Text = "Form1"
+        Me.TabControl1.ResumeLayout(False)
+        Me.TabPage1.ResumeLayout(False)
+        Me.TabPage1.PerformLayout()
+        Me.TabPage2.ResumeLayout(False)
+        Me.TabPage2.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents txt_URL As TextBox
+    Friend WithEvents WebBrowser1 As WebBrowser
+    Friend WithEvents btnSure As Button
+    Friend WithEvents TabControl1 As TabControl
+    Friend WithEvents TabPage1 As TabPage
+    Friend WithEvents TabPage2 As TabPage
+    Friend WithEvents btn_Exec As Button
+    Friend WithEvents txtDisplay As TextBox
+End Class
---
+++ VB_WinForm/MainFrm.vb
@@ -0,0 +1,56 @@
+﻿
+Imports System.Environment
+Imports System.Linq
+Imports Entity
+
+
+Public Class MainFrm
+    Private Sub btnSure_Click(sender As Object, e As EventArgs) Handles btnSure.Click
+        If txt_URL.Text.Trim() IsNot Nothing Then
+            WebBrowser1.Url = New Uri(txt_URL.Text)
+        End If
+
+
+    End Sub
+
+    Private Sub btn_Exec_Click(sender As Object, e As EventArgs) Handles btn_Exec.Click
+        Dim str_b = $"現在は:{DateTime.Now.ToString("yyyyMMdd")}"
+        Dim str_a = Procedure(str_b)
+
+        Dim module_1 = New [Module]()
+        Dim entity = New Entity.PZDBEntities()
+
+        Dim moduleList As List(Of [Module])
+        moduleList = (From m In entity.Modules
+                      Where m.ModuleCode IsNot Nothing
+                      Select m).ToList()
+
+        moduleList = entity.Modules.Where(Function(m) m.ModuleCode IsNot Nothing).ToList()
+
+        For Each mo As [Module] In moduleList
+            Dim str_Line = ""
+            For Each prop As System.Reflection.PropertyInfo In mo.GetType().GetProperties()
+                Dim obj_value As String
+                obj_value = If(prop.GetValue(mo) Is Nothing, " ", prop.GetValue(mo).ToString())
+                str_Line += $"{obj_value},"
+            Next
+            Procedure(str_Line.Substring(0, str_Line.Length - 1))
+        Next
+
+    End Sub
+
+
+
+
+    Function Procedure(ByVal strLine As String) As Boolean
+        Dim opBool = True
+        Try
+            txtDisplay.Text = $"{strLine}{NewLine}{txtDisplay.Text}"
+        Catch ex As Exception
+            txtDisplay.Text = $"{ex.Message}{NewLine}{txtDisplay.Text}"
+            opBool = False
+        End Try
+        Return opBool
+    End Function
+
+End Class
